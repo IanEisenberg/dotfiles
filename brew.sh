16da8ba0081485496effc08de73f8394e14f7c9b
Ian Eisenberg <ianeisenberg90@gmail.com>
2019-12-20 13:07:28 -0800

    first commit, mostly copying from CoreyMSchafer

---
+++ brew.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+brew install python
+brew install tree
+
+# Install MacOS Applications
+# Professional
+brew cask install google-chrome
+brew cask install sublime-text
+brew cask install spotify
+
+# Personal
+brew cask install google-backup-and-sync
+brew cask install discord
+brew cask install vlc
+
